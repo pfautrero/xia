011b7207771a466fb385a633dd0c466ab43bbd7b
lafont <francois.lafont@crdp.ac-versailles.fr>
2014-10-05 00:35:28 +0200

    Update setup.py and MANIFEST.in
    Necessary after refactoring.

--- src/setup.py
+++ src/setup.py
@@ -17,23 +17,51 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-from setuptools import setup, find_packages
+import os
+from distutils.core import setup
+from setuptools import find_packages
+
+# The directory which contains this script.
+setup_dir = os.path.dirname(os.path.abspath(__file__))
+changelog = os.path.join(setup_dir, 'CHANGELOG.md')
+readme = os.path.join(setup_dir, '../README.md')
 
 # Get the version of the application.
-with open('CHANGELOG.md', 'r') as f:
+with open(changelog, 'r') as f:
   line1 = f.readline()
 words = line1.split()
 version = words[1]
 
+# Get the long description of the application.
+with open(readme, 'r') as f:
+    long_description = f.read()
+
 setup(
-    name='xia-converter',
+    name='xiaconverter',
     version=version,
+    # Search all python packages in the root of setup_dir
+    # (all directories which contain a __init__.py file).
     packages=find_packages(),
+
+    # With this parameter, a non python file in a package and
+    # found in MANIFEST.in will be included in the build with:
+    #
+    #   ./setup.py install --root=/tmp/foo --install-layout=deb
+    #
+    # But files in MANIFEST.in and not in a package will be
+    # not included in the build with the command above.
+    #
+    # To create tar.gz which includes all files in MANIFEST.in,
+    # use this command:
+    #
+    #   ./setup.py sdist
+    #
+    include_package_data=True,
+
     author='Pascal Fautrero',
     author_email='pascal.fautrero@ac-versailles.fr',
     description='Convert svg to full html5 interactive pictures',
-    long_description=open('README.md').read(),
-    include_package_data=True,
+    long_description=long_description,
     url='http://images-actives.crdp-versailles.fr/beta/',
     license='GPL-3',
 )
