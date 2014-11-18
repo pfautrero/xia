dc78f79a4046df006ea098f0daee5ec80f8778d7
Michaël Nourry <michael.nourry@crdp.ac-versailles.fr>
2014-11-18 09:55:20 +0100

    add  tests click

--- tests/functional_test/accordion/test.py
+++ tests/functional_test/accordion/test.py
@@ -53,6 +53,38 @@ class Test(unittest.TestCase):
         driver.find_element_by_xpath("//div[@id='canvas']/div/canvas[4]").click()
         driver.find_element_by_id("collapse5-heading").click()
         driver.find_element_by_id("collapse6-heading").click() 
+        
+    dev test_click(self):
+        driver = self.driver
+        driver.get(self.base_url)
+        try: self.assertTrue(driver.find_element_by_xpath("//div[@id='collapse4']/div/hr").is_displayed())
+        except AssertionError as e: self.verificationErrors.append(str(e))
+        try: self.assertEqual("Description de beziertracer \n une ligne", driver.find_element_by_xpath("//div[@id='collapse4']/div").text)
+        except AssertionError as e: self.verificationErrors.append(str(e))
+        driver.find_element_by_css_selector("a.infos").click()
+        driver.find_element_by_id("popup_close").click()
+        driver.find_element_by_id("collapse0-heading").click()
+        driver.find_element_by_id("collapse1-heading").click()
+        driver.find_element_by_id("collapse2-heading").click()
+        driver.find_element_by_id("collapse4-heading").click()
+        driver.find_element_by_id("collapse5-heading").click()
+        driver.find_element_by_id("collapse6-heading").click()
+        driver.find_element_by_id("collapsecomment-heading").click()
+        driver.find_element_by_id("collapsecomment-heading").click()
+        driver.find_element_by_id("collapse0-heading").click()
+        driver.find_element_by_css_selector("#collapse0 > div.accordion-inner").click()
+        self.assertEqual("Description du rectangle gras italiqueRéponse:Voici la vidéo :", driver.find_element_by_css_selector("#collapse0 > div.accordion-inner").text)
+        driver.find_element_by_id("collapsecomment-heading").click()
+        driver.find_element_by_id("collapse0-heading").click()
+        self.assertEqual("Description de l'image", driver.find_element_by_css_selector("div.accordion-inner").text)
+        try: self.assertTrue(self.is_element_present(By.CSS_SELECTOR, "#collapse0 > div.accordion-inner > video"))
+        except AssertionError as e: self.verificationErrors.append(str(e))
+        self.assertEqual("", driver.find_element_by_css_selector("#collapse0 > div.accordion-inner > video").text)
+        self.assertEqual("Description de beziertracer \n une ligne", driver.find_element_by_css_selector("#collapse4 > div.accordion-inner").text)
+         = driver.find_element_by_css_selector("#collapse4 > div.accordion-inner").text
+         try: self.assertEqual("Description de beziertracer \n une ligne", driver.find_element_by_css_selector("#collapse4 > div.accordion-inner").text)
+         except AssertionError as e: self.verificationErrors.append(str(e))
+         driver.find_element_by_id("collapse4-heading").click()
 
     def is_element_present(self, how, what):
         try: self.driver.find_element(by=how, value=what)
